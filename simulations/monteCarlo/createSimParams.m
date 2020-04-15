d25a524aa45b822a9a4a7dc198463a14adfa6db3
Tyler Gardner <tyguy609@gmail.com>
2020-04-15 16:17:02 -0600

    Adjusted the createSimParams function to match the new config file structure

--- simulations/monteCarlo/createSimParams.m
+++ simulations/monteCarlo/createSimParams.m
@@ -1,51 +1,65 @@
 function [ simparams, simparamsref ] = createSimParams( parameterFile)
 %Read in general simulation parameters
-[~,names,raw] = xlsread(parameterFile,'general');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.general.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'general');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.general.(string(table{i,1})) = table{i,5};
+end
+
+%Read in constants
+table = readtable(parameterFile, 'Sheet', 'constantParams');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.constants.(string(table{i,1})) = table{i,5};
 end
 
 %Read in initial conditions
-[~,names,raw] = xlsread(parameterFile,'initialConditions');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.init.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'initialConditions');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.init.(string(table{i,1})) = table{i,5};
+end
+
+%Read in rocket properties
+table = readtable(parameterFile, 'Sheet', 'rocketProps');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.rocket.(string(table{i,1})) = table{i,5};
 end
 
 %Read in truth state parameters
-[~,names,raw] = xlsread(parameterFile,'truthStateParams');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.truth.params.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'truthStateParams');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.truth.params.(string(table{i,1})) = table{i,5};
 end
 
 %Read in truth state initial uncertainty
-[~,names,raw] = xlsread(parameterFile,'truthStateInitialUncertainty');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.truth.ic.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'truthStateInitialUncertainty');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.truth.ic.(string(table{i,1})) = table{i,5};
 end
 
 %Read in navigation state parameters
-[~,names,raw] = xlsread(parameterFile,'navStateParams');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.nav.params.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'navStateParams');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.nav.params.(string(table{i,1})) = table{i,5};
 end
 
 %Read in navigation state initial uncertainty
-[~,names,raw] = xlsread(parameterFile,'navStateInitialUncertainty');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.nav.ic.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'navStateInitialUncertainty');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.nav.ic.(string(table{i,1})) = table{i,5};
 end
 
 %Read error injection
-[~,names,raw] = xlsread(parameterFile,'errorInjection');
-[num_params,~] = size(raw);
-for i=2:num_params
-    simparams.errorInjection.(names{i}) = raw{i,5};
+table = readtable(parameterFile, 'Sheet', 'errorInjection');
+[num_params,~] = size(table);
+for i=1:num_params
+    simparams.errorInjection.(string(table{i,1})) = table{i,5};
 end
 
 %Create reference trajectory simparams (with all process noise, initial
