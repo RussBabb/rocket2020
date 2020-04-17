3902dc019abe727f95196c9e390ae0792c462b9d
Tyler Gardner <tyguy609@gmail.com>
2020-04-17 17:40:12 -0600

    Updated the gravity function to have latitude as a parameter

--- simulations/monteCarlo/Utilities/calcGrav.m
+++ simulations/monteCarlo/Utilities/calcGrav.m
@@ -1,8 +1,8 @@
-function g = calcGrav(h)
+function g = calcGrav(h, lambda)
 %calcGrav Calculates the graviatational acceleration magnitude  for a given
 %altitude
 %   h = altutude above mean sea level (m)
-global lambda
+%   lambda = launch latitude
 MU = 3.98060044e5;      %km^3/s^2
 R_EQ = 6378.13649;      %km
 omega = 7.2929115e-5;
