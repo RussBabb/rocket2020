2ada57d5c8ff8b9080bfdbfcf1780ca0058ecd01
Tyler Gardner <tyguy609@gmail.com>
2020-04-20 13:49:08 -0600

    Added function to calculate gravity partial derivative

--- simulations/monteCarlo/Utilities/calcdGrav.m
+++ simulations/monteCarlo/Utilities/calcdGrav.m
@@ -1,6 +1,6 @@
-function g = calcGrav(h, lat)
-%calcGrav Calculates the graviatational acceleration magnitude  for a given
-%altitude and latitude
+function dg = calcdGrav(h, lat)
+%calcGrav Calculates the derivative of the graviatational acceleration for 
+%a given altitude and latitude
 %   h = altutude above mean sea level (m)
 %   lat = launch latitude
 MU = 3.98060044e5;      %km^3/s^2
@@ -9,5 +9,5 @@ omega = 7.2929115e-5;
 e = 0.08181980;
 
 R_EARTH = R_EQ/sqrt(1 + e^2/(1 - e^2)*sin(lat)^2);
-g = (MU/(R_EARTH + h/1000)^2 - omega^2*(R_EARTH + h/1000))*1000;
+dg = (1/(R_EARTH + h/1000)^2 - 2*MU/(R_EARTH + h/1000)^3 - omega^2)*1000;
 end
